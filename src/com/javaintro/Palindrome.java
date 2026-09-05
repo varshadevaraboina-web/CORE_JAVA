4fa4713f92f05deaa6afd347a086b1e60422b4fb
ADMIN <ADMIN@DESKTOP-O7E9H2A>
2026-09-05 09:07:37 +0530

    My palindrome code.

--- src/com/javaintro/Palindrome.java
+++ src/com/javaintro/Palindrome.java
@@ -5,20 +5,24 @@ public class Palindrome {
 	public static void main(String[] args) {
 		String str="wow";
 		String rev="";
-		for(int i=str.length()-1;i>=0;i--)
-		{
+		for(int i=str.length()-1;i>=0;i--) {
 			rev=rev+str.charAt(i);
-			}
-		
-		if(str.equals(rev))
-		{
+			
+		}
+		if(str.equals(rev)){
 			System.out.println("The String is Palindrome:" +rev);
+			
 		}
 		else {
 			
-			System.out.println("The String is not Palindrome:" +rev);
+			System.out.println("The String is Not Palindrome:" +rev);
 		}
-		
-		}
-	
+
+	}
+
 }
+
+
+	
+
+
