8edc0536c958831c79766b0e55a5b64c44a2efab
varsha <varshadevaraboina@gmail.com>
2026-09-14 00:11:15 +0530

    Add SmallestNumber program

---
+++ src/com/javaintro/SmallestNumber.java
@@ -0,0 +1,23 @@
+package com.javaintro;
+
+public class SmallestNumber {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=3;
+		int c=16;
+		if(a<b && a<c ) {
+		System.out.println(" a is smallest number:" +" "+a);
+		}
+		else if(b<a && b<c){
+			System.out.println(" b is smallest number:" +" "+b);
+			
+		}
+		if(c<a && c<b) {
+			
+		System.out.println(" c is smallest number:" +" "+c);
+		}
+
+	}
+
+}
