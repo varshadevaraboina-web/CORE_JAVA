4012903c491c90f9d37d94351d8e56c735d62824
varsha <varshadevaraboina@gmail.com>
2026-09-14 23:28:15 +0530

    EmployeeAddress program

---
+++ src/com/javaintro/EmployeeAddress.java
@@ -0,0 +1,30 @@
+package com.javaintro;
+
+
+class Address{
+	String name="varsha";
+	int rollno=103;
+	String address="Hyd";
+	}
+
+public class EmployeeAddress {
+	String empName="Ram";
+	byte empId=124;
+	int sal=50000;
+	Address a=new Address();
+	
+	public static void main(String[] args) {
+		EmployeeAddress e=new EmployeeAddress();
+		System.out.println("Employee Details");
+		System.out.println("Name:" +" "+e.empName);
+		System.out.println("ID:" +" "+e.empId);
+		System.out.println("Salary:" +" "+e.sal);
+		System.out.println(" ");
+		System.out.println("Student Details");
+		System.out.println("Name:" +" "+e.a.name);
+		System.out.println("Roll Number:" +" "+e.a.rollno);
+		System.out.println("Address:" +" "+e.a.address);
+
+	}
+
+}
